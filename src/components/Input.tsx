3fa2e35c0dda487715060d39a496bd8c35a39d81
LONEWOLF143 <twaha2030@gmail.com>
2023-07-14 12:33:44 +0300

    useState hook

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React from "react";
 
 type InputProps = {
-    value: string
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+function Input({ value, handleChange }: InputProps) {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log(event);
+  };
+  
+  return <input type="text" value={value} onChange={handleChange} />;
 }
 
-function Input({value, handleChange}: InputProps) {
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => void
-    console.log(event)
-  return <input type='text' value={value} onChange={handleChange}/>
-}
-
-export default Input
+export default Input;
